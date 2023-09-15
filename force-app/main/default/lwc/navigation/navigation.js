35ce72d41b39bc6777d94448698ae7898baf591f
Gautam-012 <gk601@gmail.com>
2023-09-15 08:53:36 +0530

    Initiate for

---
+++ force-app/main/default/lwc/navigation/navigation.js
@@ -0,0 +1,61 @@
+import { LightningElement} from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
+
+//(encapsulate) for the funcationality of the NavigateMixin 
+export default class Navigation extends NavigationMixin(LightningElement) {
+
+    handlenavigate(){
+        //navigate home to account obj
+        // console.log('Navigated to Account Page')
+        // this[NavigationMixin.Navigate]({
+        //     type: 'standard__objectPage',
+        //     attributes: {
+        //         objectApiName:'Account',
+        //         actionName: 'home' 
+        //     }
+        // })
+
+
+    // navigate home to recent records of account
+        // this[NavigationMixin.Navigate]({
+        //     type: 'standard__objectPage',
+        //     attributes: {
+        //         objectApiName:'Account',
+        //         actionName: 'list' 
+        //     },
+        //     state:{
+        //         filterName: 'Recent'
+        //     }
+        // })
+
+        // navigate home to account while  creating new record
+        // this[NavigationMixin.Navigate]({
+        //     type: 'standard__objectPage',
+        //     attributes: {
+        //         objectApiName:'Account',
+        //         actionName: 'new' 
+        //     }
+        // })
+
+        // navigate home to specific records of account ID
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: "a001025j00000b635cAAA",
+                objectApiName: 'Merchandise__c',
+                actionName: 'view'
+            }
+        });
+
+        // Navigate to Edit Account Page
+        // this[NavigationMixin.Navigate]({
+        //     type: 'standard__recordPage',
+        //     attributes: {
+        //         recordId: this.recordId,
+        //         objectApiName: 'Account',
+        //         actionName: 'edit'
+        //     },
+        // });
+    }
+        
+}
